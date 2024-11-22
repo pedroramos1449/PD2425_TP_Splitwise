11bf4671e03b9f30cdc1517e53ce114610c32877
André Ladeiro <59754141+QuackPotato@users.noreply.github.com>
2024-11-22 17:56:42 +0000

    Update init.sql

--- target/classes/init.sql
+++ target/classes/init.sql
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS grupos (
                                       FOREIGN KEY (id_utilizador_criador) REFERENCES utilizadores(id)
     );
 
-CREATE TABLE IF NOT EXISTS membros (
-                                       id_grupo INTEGER NOT NULL,
-                                       id_utilizador INTEGER NOT NULL,
-                                       PRIMARY KEY (id_grupo, id_utilizador),
-                                       FOREIGN KEY (id_grupo) REFERENCES grupos(id),
-                                       FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id)
+CREATE TABLE IF NOT EXISTS despesas_partilhadas (
+                                         id_despesa INTEGER NOT NULL,
+                                         id_utilizador INTEGER NOT NULL,
+                                         valor REAL NOT NULL,
+                                         PRIMARY KEY (id_despesa, id_utilizador),
+                                         FOREIGN KEY (id_despesa) REFERENCES despesas(id),
+                                         FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id)
 );
-
 
 CREATE TABLE IF NOT EXISTS despesas (
                                         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +32,24 @@ CREATE TABLE IF NOT EXISTS despesas (
                                         FOREIGN KEY (id_grupo) REFERENCES grupos(id),
                                         FOREIGN KEY (id_utilizador_pagador) REFERENCES utilizadores(id)
     );
+
+CREATE TABLE IF NOT EXISTS membros (
+                                       id_grupo INTEGER NOT NULL,
+                                       id_utilizador INTEGER NOT NULL,
+                                       PRIMARY KEY (id_grupo, id_utilizador),
+                                       FOREIGN KEY (id_grupo) REFERENCES grupos(id),
+                                       FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id)
+);
+
+CREATE TABLE IF NOT EXISTS pedidos_grupo (
+                                             id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                             id_grupo INTEGER NOT NULL,
+                                             id_utilizador INTEGER NOT NULL,
+                                             id_remetente INTEGER NOT NULL,
+                                             status TEXT NOT NULL DEFAULT 'pendente',
+                                             FOREIGN KEY (id_grupo) REFERENCES grupos (id),
+                                             FOREIGN KEY (id_utilizador) REFERENCES utilizadores (id)
+);
 
 CREATE TABLE IF NOT EXISTS versao_bd (
                                          versao INTEGER PRIMARY KEY
